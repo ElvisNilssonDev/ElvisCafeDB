e58d473d4800ef71478efde942ce1557da2c8290
Elvis Nilsson <Nilssonelvis@gmail.com>
2025-12-12 15:43:49 +0100

    Delete
    # Added
    Delete to destroy myself out of the database

---
+++ SQL/Delete.sql
@@ -0,0 +1,6 @@
+DELETE FROM Customers
+WHERE FirstName = 'Elvis' AND LastName = 'Nilsson';
+
+--Incase I didnt like myself then I could delete myself like this 
+
+--WE NEED TO USE WHERE when deleting something! if we dont then we delete everything.
