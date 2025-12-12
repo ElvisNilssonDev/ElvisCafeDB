009ddcca0d886ab52f16e8c013c8a125b4a10519
Elvis Nilsson <Nilssonelvis@gmail.com>
2025-12-12 15:37:43 +0100

    Updates!!
    # Added
    Updates to some of our tables like my name and the coffee prices

---
+++ SQL/Uppdates.sql
@@ -0,0 +1,11 @@
+UPDATE Customers
+SET LastName = 'Nilsson'
+WHERE FirstName = 'Elvis' AND LastName = 'Larson';
+
+-- Well because my name isnt Elvis Larsson I will change it to my REAL Lastname Nilsson
+
+UPDATE Products
+SET Price = Price + 5000
+WHERE Category = 'Coffee';
+
+-- Inflation struck unfortunetly :<
